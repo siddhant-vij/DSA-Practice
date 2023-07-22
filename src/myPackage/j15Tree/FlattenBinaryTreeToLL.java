e3c683ad3c5ddb8cfc4bdfbe8165b7541b7c85d4
Siddhant Vij <siddhantvij201@gmail.com>
2023-07-22 12:49:56 +0530

    Leetcode: 114. Flatten Binary Tree to Linked List

---
+++ src/myPackage/j15Tree/FlattenBinaryTreeToLL.java
@@ -0,0 +1,90 @@
+// Leetcode: 114. Flatten Binary Tree to Linked List
+
+package myPackage.j15Tree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class FlattenBinaryTreeToLL {
+
+  static class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+  static TreeNode buildTree(String str) {
+    if (str.length() == 0 || str.charAt(0) == 'N') {
+      return null;
+    }
+    String ip[] = str.split(" ");
+    TreeNode root = new TreeNode(Integer.parseInt(ip[0]));
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.add(root);
+    int i = 1;
+    while (queue.size() > 0 && i < ip.length) {
+      TreeNode currNode = queue.peek();
+      queue.remove();
+      String currVal = ip[i];
+      if (!currVal.equals("N")) {
+        currNode.left = new TreeNode(Integer.parseInt(currVal));
+        queue.add(currNode.left);
+      }
+      i++;
+      if (i >= ip.length)
+        break;
+      currVal = ip[i];
+      if (!currVal.equals("N")) {
+        currNode.right = new TreeNode(Integer.parseInt(currVal));
+        queue.add(currNode.right);
+      }
+      i++;
+    }
+    return root;
+  }
+
+  static List<Integer> levelOrderTraversal(TreeNode root, List<Integer> result) {
+    if (root == null)
+      return result;
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.add(root);
+    while (!queue.isEmpty()) {
+      TreeNode curNode = queue.remove();
+      result.add(curNode.val);
+      if (curNode.left != null)
+        queue.add(curNode.left);
+      if (curNode.right != null)
+        queue.add(curNode.right);
+    }
+    return result;
+  }
+
+  static TreeNode prev = null;
+
+  static void flatten(TreeNode root) {
+    if (root == null)
+      return;
+    flatten(root.right);
+    flatten(root.left);
+    root.right = prev;
+    root.left = null;
+    prev = root;
+  }
+
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      String str = sc.nextLine();
+      TreeNode root = buildTree(str);
+      flatten(root);
+      System.out.println(levelOrderTraversal(root, new ArrayList<>()));
+    }
+  }  
+}
