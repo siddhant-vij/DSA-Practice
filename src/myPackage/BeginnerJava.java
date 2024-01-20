2b21ae4fb21b41f21cf6b66b926ba376a2c3b5e9
Siddhant Vij <siddhantvij201@gmail.com>
2024-01-20 18:25:48 +0530

    Trial Commit

--- src/myPackage/BeginnerJava.java
+++ src/myPackage/BeginnerJava.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class BeginnerJava {
   public static void main(String[] args) {
     try (Scanner sc = new Scanner(System.in)) {
-      int x = sc.nextInt();
-      System.out.println("Hello World " + x);
+      System.out.println("Hello World ");
     }
   }
 }
